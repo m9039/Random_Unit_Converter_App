d9ae6a8f86b3c30f71dc69ec71c09daf9b5bd6bc
m9039 <61225254+m9039@users.noreply.github.com>
2021-09-28 21:28:10 +1000

    second activity UI

---
+++ app/src/main/java/au/edu/unsw/infs3634/unitconverter/SecondActivity.java
@@ -0,0 +1,14 @@
+package au.edu.unsw.infs3634.unitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
