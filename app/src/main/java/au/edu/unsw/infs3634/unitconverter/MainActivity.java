c96e2373437f58a6782605a49cc1c848a5f6754c
m9039 <61225254+m9039@users.noreply.github.com>
2021-09-29 21:17:16 +1000

    generateUnits successful

--- app/src/main/java/au/edu/unsw/infs3634/unitconverter/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/unitconverter/MainActivity.java
@@ -1,8 +1,10 @@
 package au.edu.unsw.infs3634.unitconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+import java.util.Random;
+import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //asjkdhajks
+
+        generateUnits();
+    }
+
+    public void generateUnits(){
+        String[] units = {"kilometres", "metres", "centimetres", "millimetres", "miles", "yards", "feet", "inches"};
+
+        Random random = new Random();
+
+        int[] textViews = new int[2];
+        textViews[0] = R.id.tvUnit1;
+        textViews[1] = R.id.tvUnit2;
+
+        Vector<String> alreadyUsed = new Vector<String>();
+
+        for(int v : textViews){
+            TextView tv = (TextView)findViewById(v);
+            String nextString;
+            do {
+                nextString = units[random.nextInt(units.length)];
+            } while (alreadyUsed.contains(nextString));
+            alreadyUsed.add(nextString);
+            tv.setText(nextString);
+        }
     }
 }
+
+
